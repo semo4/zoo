f81aeaffc11bf137d21b8cc82d037049e4621eef
osama-yousef1993 <osama.yousef@optimumpartners.com>
2023-12-24 20:08:26 +0300

    add name

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,1 @@
-echo "Building project packages ...."
-python3 -m pip3 install -r requirements.txt
-
-echo "Migrating Database ...."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-echo "Collecting static files ...."
 python3 manage.py collectstatic --noinput
